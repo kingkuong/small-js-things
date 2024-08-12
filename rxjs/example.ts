c56896e3263056903feadd10303c578faa996c1f
Cuong Tran <cuongtran92@gmail.com>
2024-08-12 17:19:01 -0400

    fix comments

--- rxjs/example.ts
+++ rxjs/example.ts
@@ -16,12 +16,12 @@ const genericSubscriber: Subscriber = {
 console.log("Example from([1,2,3])");
 from([1, 2, 3]).subscribe(genericSubscriber);
 
-console.log("Example of(1,2,3)");
+console.log("Example of(1,2,3,4,5,6,7,8,9,10)");
 of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   .pipe([filter((value) => value % 2 === 0), map((value) => value * 2)])
   .subscribe(genericSubscriber);
 
-console.log("Example interval(1000)");
+console.log("Example interval(500, 5)");
 const interval$ = interval(500, 5);
 const subscription$ = interval$.subscribe(genericSubscriber);
 setTimeout(() => subscription$.unsubscribe(), 2000);
